d13cf3cdb8a98a98cef13a1fe04498593c4afe83
Ben Manekin <ben.manekin@gmail.com>
2019-02-11 23:18:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,18 @@
+# defines the method, takes an argument list
 def my_each(list)
+# initializes variable i at 0
   i = 0
+# begins while loop, sets condition of i less than
+# the length of argument list
   while i < list.length
+# sets yield block, takes argument from array
+# list at index i
     yield(list[i])
+# increments i at the end of the loop
     i += 1
+# closes the while loop
   end
+# returns the list
   list
+# closes the function definition
 end
